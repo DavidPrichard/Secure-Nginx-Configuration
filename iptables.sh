863f8f1296c65604cbbf1de2765c337b8b544d5c
David Prichard <davidprichard64@gmail.com>
2017-08-20 14:49:21 -0500

    Update: iptables rules
    Dropped unnecessary rules blocking xmas-tree and null packets, outgoing packets, etc.

--- iptables.sh
+++ iptables.sh
@@ -13,17 +13,12 @@
  iptables -P FORWARD DROP
  iptables -P OUTPUT  ACCEPT
 
-# DENY
- iptables -A INPUT -p tcp --tcp-flags ALL NONE         -j DROP # null packets
- iptables -A INPUT -p tcp --tcp-flags ALL ALL          -j DROP # Xmas tree pkts
- iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP # syn-flood pkts
-
 # ACCEPT
- iptables -A INPUT -i lo                            -j ACCEPT # localhost
- iptables -A INPUT -p tcp        --dport 22         -j ACCEPT # SSH
- iptables -A INPUT -p tcp -m tcp --dport 80         -j ACCEPT # HTTP
  iptables -A INPUT -p tcp -m tcp --dport 443        -j ACCEPT # HTTPS
+ iptables -A INPUT -p tcp -m tcp --dport 80         -j ACCEPT # HTTP
  iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT # Ping
+ iptables -A INPUT -p tcp        --dport 22         -j ACCEPT # SSH
+ iptables -A INPUT -i lo                            -j ACCEPT # localhost
 
  # Show resulting rules
  iptables -L -v
@@ -38,21 +33,12 @@
  ip6tables -P FORWARD DROP
  ip6tables -P OUTPUT  ACCEPT
 
-# DENY
- ip6tables -A INPUT -p tcp --tcp-flags ALL NONE         -j DROP # null packets
- ip6tables -A INPUT -p tcp --tcp-flags ALL ALL          -j DROP # Xmas tree pkts
- ip6tables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP # syn-flood pkts
-
 # ACCEPT incoming
- ip6tables -A INPUT -i lo                            -j ACCEPT # localhost
- ip6tables -A INPUT -p tcp        --dport 22         -j ACCEPT # SSH
- ip6tables -A INPUT -p tcp -m tcp --dport 80         -j ACCEPT # HTTP
  ip6tables -A INPUT -p tcp -m tcp --dport 443        -j ACCEPT # HTTPS
+ ip6tables -A INPUT -p tcp -m tcp --dport 80         -j ACCEPT # HTTP
  ip6tables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT # Ping
-
-# ACCEPT outgoing
- ip6tables -A OUTPUT -o lo                          -j ACCEPT # localhost
- ip6tables -A OUTPUT -p icmp --icmp-type echo-reply -j ACCEPT # Ping reply
+ ip6tables -A INPUT -p tcp        --dport 22         -j ACCEPT # SSH
+ ip6tables -A INPUT -i lo                            -j ACCEPT # localhost
 
  # Show resulting rules
  ip6tables -L -v
